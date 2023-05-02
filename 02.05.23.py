62043d3d4bce447d57244c76f0b413cf01a0fa8a
student <maksimkozlovskiy2011@gmail.com>
2023-05-02 18:41:33 +0300

    приклад обгортки функції

--- 02.05.23.py
+++ 02.05.23.py
@@ -1,1 +1,16 @@
+#decorators theory
+
+
+def my_decorator_func(func): #3
+    def wrapper(): #4
+        print("smth")
+        func()
+        print("smth again")
+    return wrapper
+
+@my_decorator_func #2
+def say_hello(): #1
+    print("Hello!")
+say_hello()
+
 
